8d72849fff0e92c562cbe6b5348de34c578b9768
Max Kohler <hello@maxkohler.com>
2023-01-27 11:08:40 +0100

    Set initial scroll position on load

--- new-work/src/hooks/useScrollPosition.js
+++ new-work/src/hooks/useScrollPosition.js
@@ -1,16 +1,17 @@
 import { useEffect, useState } from 'react'
 
 const useScrollPosition = () => {
-    const [scrollPosition, setScrollPosition] = useState({ x: 0, y: 0 })
-    useEffect(() => {
-        const updateScrollPosition = (e) => {
-            setScrollPosition({ x: window.scrollX, y: window.scrollY })
-        }
-        window.addEventListener('scroll', updateScrollPosition)
-        return () => {
-            window.removeEventListener('scroll', updateScrollPosition)
-        }
-    }, [])
-    return scrollPosition
+  const [scrollPosition, setScrollPosition] = useState({ x: 0, y: 0 })
+  useEffect(() => {
+    const updateScrollPosition = (e) => {
+      setScrollPosition({ x: window.scrollX, y: window.scrollY })
+    }
+    window.addEventListener('scroll', updateScrollPosition)
+    updateScrollPosition()
+    return () => {
+      window.removeEventListener('scroll', updateScrollPosition)
+    }
+  }, [])
+  return scrollPosition
 }
 export default useScrollPosition
