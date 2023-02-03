c1c59660db674dbfbe317d1f685931ea11d92389
Max Kohler <hello@maxkohler.com>
2023-02-03 14:28:20 +0100

    Differentiate better between active and non-active choices

--- shared/components/EmbedChoices.js
+++ shared/components/EmbedChoices.js
@@ -4,12 +4,13 @@ import MarkdownRenderer from 'react-markdown-renderer'
 export default function EmbedChoices({ styles, providers, choices, onChange }) {
   const baseId = useId()
   const items = providers.map((p, i) => {
+    const isChecked = choices[p.name] || false
     return (
-      <li key={`${baseId}-${i}`} className={styles.choice}>
+      <li key={`${baseId}-${i}`} className={`${styles.choice} ${isChecked ? styles.checked : null}`}>
         <input
           className={styles.checkbox}
           type="checkbox"
-          checked={choices[p.name] || false}
+          checked={isChecked}
           onChange={(e) => {
             onChange(e, p.name)
           }}
