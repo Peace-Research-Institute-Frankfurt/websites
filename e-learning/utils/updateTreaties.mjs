78ac3f364049f0c19713928bc6ea8336ea4b99aa
Max Kohler <hello@maxkohler.com>
2023-02-09 17:08:27 +0100

    make update script a module

--- e-learning/utils/updateTreaties.mjs
+++ e-learning/utils/updateTreaties.mjs
@@ -8,59 +8,59 @@
 // @TODO: This should just read treaty URLs from treaties.json,
 // and not create whole new entries.
 
-import { DateTime } from "luxon"
-import puppeteer from "puppeteer"
-import fs from "fs"
-import path from "path"
-import { diff } from "just-diff"
-import { exit } from "process"
-import { fileURLToPath } from "url"
+import { DateTime } from 'luxon'
+import puppeteer from 'puppeteer'
+import fs from 'fs'
+import path from 'path'
+import { diff } from 'just-diff'
+import { exit } from 'process'
+import { fileURLToPath } from 'url'
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
-const treatiesFile = path.join(__dirname, "../content/data/treaties.json")
+const treatiesFile = path.join(__dirname, '../content/data/treaties.json')
 console.log(`Reading existing treaties from ${treatiesFile}`)
 
-const countries = JSON.parse(fs.readFileSync(path.join(__dirname, "../content/data/countries.json")))
+const countries = JSON.parse(fs.readFileSync(path.join(__dirname, '../content/data/countries.json')))
 const treaties = JSON.parse(fs.readFileSync(treatiesFile))
 
 const pages = [
-  { treaty: "cwc", url: "https://treaties.un.org/Pages/ViewDetails.aspx?src=TREATY&mtdsg_no=XXVI-3&chapter=26" },
+  { treaty: 'cwc', url: 'https://treaties.un.org/Pages/ViewDetails.aspx?src=TREATY&mtdsg_no=XXVI-3&chapter=26' },
   // { treaty: "tpnw", url: "https://treaties.un.org/Pages/ViewDetails.aspx?src=IND&mtdsg_no=XXVI-9&chapter=26&clang=_en" },
   // { treaty: "ccw", url: "https://treaties.un.org/Pages/ViewDetails.aspx?src=IND&mtdsg_no=XXVI-2&chapter=26&clang=_en" },
 ]
 
 const nameSubs = {
-  Türkiye: "Turkey",
-  "Bolivia (Plurinational State of)": "Bolivia",
-  "Brunei Darussalam": "Brunei",
-  "Cabo Verde": "Cape Verde",
-  Congo: "Republic of the Congo",
-  "Côte d'Ivoire": "Ivory Coast",
-  "Czech Republic": "Czechia",
-  "Democratic Republic of the Congo": "DR Congo",
-  "Holy See": "Vatican City",
-  "Iran (Islamic Republic of)": "Iran",
-  "Lao People's Democratic Republic": "Laos",
-  "Micronesia (Federated States of)": "Micronesia",
-  "Republic of Korea": "South Korea",
-  "Republic of Moldova": "Moldova",
-  "Russian Federation": "Russia",
-  "Sao Tome and Principe": "São Tomé and Príncipe",
-  "St. Kitts and Nevis": "Saint Kitts and Nevis",
-  "St. Lucia": "Saint Lucia",
-  "St. Vincent and the Grenadines": "Saint Vincent and the Grenadines",
-  "State of Palestine": "Palestine",
-  "Syrian Arab Republic": "Syria",
-  "United Kingdom of Great Britain and Northern Ireland": "United Kingdom",
-  "United Republic of Tanzania": "Tanzania",
-  "Venezuela (Bolivarian Republic of)": "Venezuela",
-  "Viet Nam": "Vietnam",
+  Türkiye: 'Turkey',
+  'Bolivia (Plurinational State of)': 'Bolivia',
+  'Brunei Darussalam': 'Brunei',
+  'Cabo Verde': 'Cape Verde',
+  Congo: 'Republic of the Congo',
+  "Côte d'Ivoire": 'Ivory Coast',
+  'Czech Republic': 'Czechia',
+  'Democratic Republic of the Congo': 'DR Congo',
+  'Holy See': 'Vatican City',
+  'Iran (Islamic Republic of)': 'Iran',
+  "Lao People's Democratic Republic": 'Laos',
+  'Micronesia (Federated States of)': 'Micronesia',
+  'Republic of Korea': 'South Korea',
+  'Republic of Moldova': 'Moldova',
+  'Russian Federation': 'Russia',
+  'Sao Tome and Principe': 'São Tomé and Príncipe',
+  'St. Kitts and Nevis': 'Saint Kitts and Nevis',
+  'St. Lucia': 'Saint Lucia',
+  'St. Vincent and the Grenadines': 'Saint Vincent and the Grenadines',
+  'State of Palestine': 'Palestine',
+  'Syrian Arab Republic': 'Syria',
+  'United Kingdom of Great Britain and Northern Ireland': 'United Kingdom',
+  'United Republic of Tanzania': 'Tanzania',
+  'Venezuela (Bolivarian Republic of)': 'Venezuela',
+  'Viet Nam': 'Vietnam',
 }
 
 const eventTypes = {
-  a: "accession",
-  A: "acceptance",
-  d: "succession",
+  a: 'accession',
+  A: 'acceptance',
+  d: 'succession',
 }
 
 let out = []
@@ -82,7 +82,7 @@ for (let i = 0; i < pages.length; i++) {
   const browser = await puppeteer.launch()
   const page = await browser.newPage()
 
-  page.on("console", async (msg) => {
+  page.on('console', async (msg) => {
     const msgArgs = msg.args()
     for (let i = 0; i < msgArgs.length; ++i) {
       console.log(await msgArgs[i].jsonValue())
@@ -91,28 +91,28 @@ for (let i = 0; i < pages.length; i++) {
 
   console.log(`Opening ${p.url}`)
   await page.goto(p.url)
-  const container = await page.waitForSelector("#participants")
+  const container = await page.waitForSelector('#participants')
 
   const participants = await container.evaluate((el, eventTypes) => {
     const participants = []
 
     // Slice off the header row
-    const rows = Array.from(el.querySelectorAll("tr")).slice(1)
+    const rows = Array.from(el.querySelectorAll('tr')).slice(1)
 
     rows.forEach((row) => {
-      const [countryEl, signedEl, treatyEventEl] = row.querySelectorAll("td")
+      const [countryEl, signedEl, treatyEventEl] = row.querySelectorAll('td')
       const events = []
 
       // Is the country a signatory?
-      if (signedEl.innerText.trim() !== "") {
-        events.push({ type: "signature", date: signedEl.innerText.trim().replace("\n", "") })
+      if (signedEl.innerText.trim() !== '') {
+        events.push({ type: 'signature', date: signedEl.innerText.trim().replace('\n', '') })
       }
 
       // Has the country ratified/accepted/etc. the treaty?
-      if (treatyEventEl.innerText.trim() !== "") {
+      if (treatyEventEl.innerText.trim() !== '') {
         const matches = treatyEventEl.innerText.trim().match(/(\d+ [a-zA-Z]+ \d+) ?([a-zA-Z]?)/)
-        let eventType = "ratification"
-        let eventDate = ""
+        let eventType = 'ratification'
+        let eventDate = ''
         if (matches && matches[1]) {
           eventDate = matches[1]
         }
@@ -122,7 +122,7 @@ for (let i = 0; i < pages.length; i++) {
         events.push({ type: eventType, date: eventDate })
       }
       participants.push({
-        name: countryEl.innerText.replace(/(\d)|(\\n)/g, "").trim(),
+        name: countryEl.innerText.replace(/(\d)|(\\n)/g, '').trim(),
         events: events,
       })
     })
@@ -142,7 +142,7 @@ for (let i = 0; i < pages.length; i++) {
       return []
     }
     const events = p.events.map((e) => {
-      return { ...e, date: DateTime.fromFormat(e.date, "d MMM yyyy").toFormat("yyyy-MM-dd") }
+      return { ...e, date: DateTime.fromFormat(e.date, 'd MMM yyyy').toFormat('yyyy-MM-dd') }
     })
     return { cca2: country.cca2, events: events }
   })
@@ -157,6 +157,6 @@ if (differences.length === 0) {
   exit(2)
 } else {
   console.log(`Treaty data has changed, writing new data ...`)
-  fs.writeFileSync(treatiesFile, `${JSON.stringify(out, null, "  ")}\n`, "utf-8")
+  fs.writeFileSync(treatiesFile, `${JSON.stringify(out, null, '  ')}\n`, 'utf-8')
   exit(0)
 }
