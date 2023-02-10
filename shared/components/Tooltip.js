4d7e36352238555ed30d2e9362a9df9f03e46a76
Max Kohler <hello@maxkohler.com>
2023-02-10 11:04:37 +0100

    Switch tooltip position when it's out of the viewport

--- shared/components/Tooltip.js
+++ shared/components/Tooltip.js
@@ -18,35 +18,28 @@ export default function Tooltip({ styles, active, position, children, id, target
 
   let containerStyles = {}
   let arrowStyles = {}
+  let arrowClass = null
   if (targetEl) {
     const tr = targetRect
     const cr = containerRef.current.getBoundingClientRect()
     const padding = 10
     const xClamped = clamp(padding, tr.x + tr.width / 2 - cr.width / 2, window.innerWidth - cr.width - tr.width / 2 - padding)
-    const yClamped = Math.max(tr.height + 100, tr.y - 12)
-
-    if (position === 'topCenter') {
-      containerStyles = { transform: `translateY(${yClamped}px) translateY(-100%) translateX(${xClamped}px)` }
-      arrowStyles = { left: `${tr.x - xClamped + tr.width / 2}px` }
+    let yClamped = 0
+    if (tr.y - padding < cr.height + padding) {
+      yClamped = tr.y + tr.height + cr.height + padding
+      arrowClass = styles.arrowTop
+    } else {
+      yClamped = tr.y - padding
+      arrowClass = styles.arrowBottom
     }
 
-    if (position === 'bottomRight') {
-      containerStyles = {
-        top: `${y + height + offsetY}px`,
-        right: `${document.documentElement.clientWidth - right}px`,
-      }
-    }
+    containerStyles = { transform: `translateY(${yClamped}px) translateY(-100%) translateX(${xClamped}px)` }
+    arrowStyles = { ...arrowStyles, left: `${tr.x - xClamped + tr.width / 2}px` }
   }
   return (
-    <span
-      ref={containerRef}
-      style={containerStyles}
-      id={id}
-      role="tooltip"
-      className={`${styles.container} ${styles[position]} ${active ? styles.active : ''}`}
-    >
+    <span ref={containerRef} style={containerStyles} id={id} role="tooltip" className={`${styles.container} ${active ? styles.active : ''}`}>
       {children}
-      <span style={arrowStyles} className={styles.arrow}></span>
+      <span style={arrowStyles} className={`${styles.arrow} ${arrowClass}`}></span>
     </span>
   )
 }
