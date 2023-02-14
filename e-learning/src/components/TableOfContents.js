db731c5346de205946b86c91eab135e5d2349f08
Max Kohler <hello@maxkohler.com>
2023-02-14 13:50:34 +0100

    Only show the contents if there's more than one item in it

--- e-learning/src/components/TableOfContents.js
+++ e-learning/src/components/TableOfContents.js
@@ -2,7 +2,7 @@ import React from 'react'
 import * as styles from './TableOfContents.module.scss'
 
 export default function TableOfContents({ items }) {
-  if (items) {
+  if (items && items.length > 1) {
     const listItems = items.map((item, i) => {
       return (
         <li key={`toc-${i}`} className={styles.item}>
