32e291888c159f955fef8aeeb3046002ceef8a99
Max Kohler <hello@maxkohler.com>
2023-02-15 19:52:22 +0100

    Fix icon buttons

--- shared/components/Button.js
+++ shared/components/Button.js
@@ -1,9 +1,14 @@
 import React from 'react'
 
-export default function Button({ onClick, label, icon, state, styles }) {
+export default function Button({ onClick, label, icon, hideLabel, state, styles, priority }) {
   if (!styles) styles = {}
+  if (!priority) priority = 'primary'
   return (
-    <button className={`${styles.container} ${icon && styles.hasIcon}`} disabled={state === 'disabled'} onClick={onClick}>
+    <button
+      className={`${styles.container} ${icon && styles.hasIcon} ${hideLabel && styles.hideLabel} ${styles[priority]}`}
+      disabled={state === 'disabled'}
+      onClick={onClick}
+    >
       <span className={styles.label}>{label}</span>
       {icon && <div className={styles.icon}>{icon}</div>}
     </button>
