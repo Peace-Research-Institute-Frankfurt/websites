8ff8ce4e1a12f03fbee87b498c03a30d270b364d
Max Kohler <hello@maxkohler.com>
2023-03-27 16:15:29 +0200

    Support popover triggers in <ButtonGroup>s better

--- e-learning/src/components/Popover.js
+++ e-learning/src/components/Popover.js
@@ -6,20 +6,18 @@ import * as styles from './Popover.module.scss'
 export default function Popover({ trigger, isActive, setIsActive, title, children }) {
   let containerStyles = {}
   return (
-    <>
-      <details style={containerStyles} className={`${styles.container} ${isActive ? styles.containerActive : ''}`} open={isActive}>
-        <summary className={styles.summary}>{trigger}</summary>
-        <div className={styles.content}>
-          <header className={styles.header}>
-            <span className={styles.title}>{title}</span>
-            <Button onClick={() => setIsActive(false)} label="Close" priority="ghost" icon={<CloseIcon />} size="small" hideLabel={true} />
-          </header>
-          {children}
-        </div>
-      </details>
+    <details style={containerStyles} className={`${styles.container} ${isActive ? styles.containerActive : ''}`} open={isActive}>
+      <summary className={styles.summary}>{trigger}</summary>
+      <div className={styles.content}>
+        <header className={styles.header}>
+          <span className={styles.title}>{title}</span>
+          <Button onClick={() => setIsActive(false)} label="Close" priority="ghost" icon={<CloseIcon />} size="small" hideLabel={true} />
+        </header>
+        {children}
+      </div>
       <button className={`${styles.backdrop} ${isActive && styles.backdropActive}`} onClick={() => setIsActive(false)}>
         Close
       </button>
-    </>
+    </details>
   )
 }
