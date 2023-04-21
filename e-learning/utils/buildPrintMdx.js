b1496b5dd42b4233d88d14af3c959dbc339df167
Max Kohler <hello@maxkohler.com>
2023-04-21 18:36:10 +0200

    Don't build print MDX for empty units

--- e-learning/utils/buildPrintMdx.js
+++ e-learning/utils/buildPrintMdx.js
@@ -11,49 +11,55 @@ const units = fs.readdirSync(basePath)
 // const units = ['lu-18']
 units.forEach((unit) => {
   const chapters = fs.readdirSync(`${basePath}/${unit}`)
+  const hasChapters = chapters.length > 0
   let unitData = []
 
-  chapters.forEach((chapter) => {
-    if (chapter !== 'index.mdx' && chapter !== 'assets' && chapter !== '__print.mdx') {
-      const chapterPath = `${basePath}/${unit}/${chapter}`
-      const frontmatter = gm.read(chapterPath)
-      if (frontmatter.data.title) {
-        unitData.push({
-          filename: chapter,
-          data: frontmatter.data,
-        })
+  if (hasChapters) {
+    chapters.forEach((chapter) => {
+      if (chapter !== 'index.mdx' && chapter !== 'assets' && chapter !== '__print.mdx') {
+        const chapterPath = `${basePath}/${unit}/${chapter}`
+        const frontmatter = gm.read(chapterPath)
+        if (frontmatter.data.title) {
+          unitData.push({
+            filename: chapter,
+            data: frontmatter.data,
+          })
+        }
       }
-    }
-  })
+    })
 
-  // Sort files by "order" frontmatter parameter
-  unitData.sort((a, b) => {
-    if (a.data.order > b.data.order) return 1
-    if (a.data.order < b.data.order) return -1
-    return 0
-  })
+    // Sort files by "order" frontmatter parameter
+    unitData.sort((a, b) => {
+      if (a.data.order > b.data.order) return 1
+      if (a.data.order < b.data.order) return -1
+      return 0
+    })
 
-  // Generate output
-  output = `import Chapter from '../../../src/components/PrintChapter.js'
+    // Generate output
+    output = `import Chapter from '../../../src/components/PrintChapter.js'
 
-${unitData
-  .map((chapter, i) => {
-    return `import Chapter${i} from './${chapter.filename}'`
-  })
-  .join('\n')}
+  ${unitData
+    .map((chapter, i) => {
+      return `import Chapter${i} from './${chapter.filename}'`
+    })
+    .join('\n')}
 
-${unitData
-  .map((chapter, i) => {
-    return `
-<Chapter title="${chapter.data.title}" intro="${chapter.data.intro}" order="${chapter.data.order}">
-  <Chapter${i} />
-</Chapter>`
-  })
-  .join('\n')}
-  `
+    ${unitData
+      .map((chapter, i) => {
+        return `
+        <Chapter title="${chapter.data.title}" intro="${chapter.data.intro}" order="${chapter.data.order}">
+        <Chapter${i} />
+        </Chapter>`
+      })
+      .join('\n')}
+      `
 
-  // Write the output
-  console.log(`Writing print template for ${unit}...`)
-  fs.writeFileSync(`${basePath}/${unit}/__print.mdx`, output)
+    // Write the output
+    fs.writeFileSync(`${basePath}/${unit}/__print.mdx`, output)
+    console.log(`Wrote ${unit} print MDX`)
+  } else {
+    console.log(`Skipped ${unit} (no chapters found)`)
+  }
 })
+
 console.log(`Done.`)
---
+++ e-learning/utils/buildUnitPDF.sh
@@ -0,0 +1,1 @@
+pagedjs-cli ../public/ -o result.pdf --page-size a4 --media print --outline-tags h1,h2,h3,h4,h5
