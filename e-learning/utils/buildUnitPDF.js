1d3950fa243b16423b8c7554f1d9111effb889d4
Max Kohler <hello@maxkohler.com>
2023-04-23 10:56:09 +0200

    Build out pipeline to set PDF metadata

--- e-learning/utils/buildUnitPDF.js
+++ e-learning/utils/buildUnitPDF.js
@@ -1,17 +1,10 @@
-// cd public
-// pagedjs-cli ./lu-18/print/index.html -o result.pdf --page-size a4 --media print --outline-tags h1,h2,h3,h4,h5 --style "print.css" --browserArgs "" -d
-
-// Start express, serve /public
-// Start puppeteer, navigate to each unit print view
-// Wait for js to run
-// Generate PDF
-
-// Can probably do this in parallel!
 'use strict'
 
 const fs = require('fs')
 const puppeteer = require('puppeteer')
 const express = require('express')
+const pdfLib = require('pdf-lib')
+const gm = require('gray-matter')
 
 const app = express()
 const port = 3000
@@ -20,6 +13,7 @@ console.log(`\nGenerating Unit PDFS...`)
 // Find our which units we need to make PDFs for
 const basePath = `./content/learning-units`
 const units = fs.readdirSync(basePath)
+// const units = ['lu-18']
 let printUnits = []
 let skippedUnits = []
 units.forEach((unit) => {
@@ -35,27 +29,52 @@ if (skippedUnits.length > 0) {
   console.log(`Skipping ${skippedUnits.join(', ')} (no print template found)`)
 }
 
-app.use(express.static('public'))
+app.use(express.static('./public'))
 
 var server = app.listen(port, function (err) {
   if (err) console.log('Error in Webserver setup')
   console.log(`Listening http://localhost:${port}`)
 })
 
+async function setMetaData(unit, outputPath) {
+  console.log(`Writing metadata for ${outputPath}...`)
+  // We have to parse the original Markdown
+  // again here to get the PDF metadata
+  const frontmatter = gm.read(`./content/learning-units/${unit}/index.mdx`)
+
+  // Load the unprocessed PDF
+  const pdfData = fs.readFileSync(outputPath)
+  const pdfDoc = await pdfLib.PDFDocument.load(pdfData)
+
+  // Set metadata
+  pdfDoc.setTitle(`[EUNPDC eLearning] ${frontmatter.data?.title}`, {
+    showInWindowTitleBar: true,
+    updateMetadata: true,
+  })
+  pdfDoc.setSubject(`${frontmatter.data?.intro}`)
+  pdfDoc.setAuthor('Peace Research Institute Frankfurt')
+  pdfDoc.setLanguage('en-EN')
+  const bytes = await pdfDoc.save()
+  fs.writeFileSync(outputPath, bytes)
+}
+
 ;(async () => {
   const browser = await puppeteer.launch({ args: ['--no-sandbox', '--export-tagged-pdf'] })
   const page = await browser.newPage()
   for (let i = 0; i < printUnits.length; i++) {
     const unit = printUnits[i]
+    const outputPath = `./public/static/eunpdc-${unit}.pdf`
+    // const outputPath = `./eunpdc-${unit}.pdf`
     console.log(`Generating PDF for ${unit}...`)
     await page.goto(`http://localhost:3000/${unit}/print`, { waitUntil: 'networkidle0' })
     await page.pdf({
-      path: `./public/static/eunpdc-${unit}.pdf`,
+      path: outputPath,
       format: 'A4',
       displayHeaderFooter: false,
       scale: 1,
       printBackground: true,
     })
+    setMetaData(unit, outputPath)
   }
   await browser.close()
   await server.close(function () {
