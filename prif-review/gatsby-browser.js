fcb3cbe8c341e48e01dd3c00e6172c1e7fbbebfb
Max Kohler <hello@maxkohler.com>
2023-05-11 20:44:39 +0200

    Remove text-wrap component

--- prif-review/gatsby-browser.js
+++ prif-review/gatsby-browser.js
@@ -1,11 +1,6 @@
 import React from 'react'
 import { EmbedChoicesProvider } from './src/context/EmbedChoicesContext'
-import { Provider } from 'react-wrap-balancer'
 
 export const wrapRootElement = ({ element }) => {
-  return (
-    <EmbedChoicesProvider>
-      <Provider>{element}</Provider>
-    </EmbedChoicesProvider>
-  )
+  return <EmbedChoicesProvider>{element}</EmbedChoicesProvider>
 }
