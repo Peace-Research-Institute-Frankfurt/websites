f18cbc79fe8e8eeb327d8526f68be9c93033d68d
Max Kohler <hello@maxkohler.com>
2023-08-02 15:13:25 +0200

    Build out org chart component

--- prif-review/src/components/Flowchart.js
+++ prif-review/src/components/Flowchart.js
@@ -1,14 +1,22 @@
 import React from 'react'
 import * as styles from './Flowchart.module.scss'
 
-const Flowchart = ({ children }) => {
-  return <figure className={styles.container}>{children}</figure>
+const Flowchart = ({ title, children }) => {
+  return (
+    <figure className={styles.container}>
+      {title && <figcaption className={styles.title}>{title}</figcaption>}
+      <div className={styles.nodes}>{children}</div>
+    </figure>
+  )
 }
 
-const FlowchartNode = ({ title, children }) => {
+const FlowchartNode = ({ eyebrow, title, id, children }) => {
   return (
-    <section className={styles.department}>
-      <h2 className={styles.departmentTitle}>{title}</h2>
+    <section className={`${styles.department} ${id ? styles[id] : ''}`}>
+      <h2 className={styles.departmentTitle}>
+        {eyebrow && <span className={styles.departmentEyebrow}>{eyebrow}</span>}
+        {title}
+      </h2>
       <div className={styles.departmentDescription}>{children}</div>
     </section>
   )
