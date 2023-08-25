25b1cfd77ee994ca6c178f6ccc19b86f1d489e9a
Max Kohler <hello@maxkohler.com>
2023-08-25 09:32:07 +0200

    Extend intro

--- new-work/src/pages/index.js
+++ new-work/src/pages/index.js
@@ -57,7 +57,14 @@ const Index = ({ data }) => {
         <PostHeader
           image={<StaticImage src="../images/cubes.png" alt="" layout="fullWidth" />}
           title="Neue Arbeitsformen für Wissenschaft und Forschung"
-          intro="New Work bricht Regeln auf, hinterfragt Machtverhältnisse und rückt den Menschen seinen Stärken, Bedürfnissen und Emotiononen in den Fokus."
+          intro={
+            <>
+              New Work bricht Regeln auf, hinterfragt Machtverhältnisse und rückt den Menschen seinen Stärken, Bedürfnissen und Emotiononen in den
+              Fokus. Diese Handreichung sammelt Anleitungen und Resourcen zu <span className={styles.introSpace}>Raum</span>,{' '}
+              <span className={styles.introPeople}>Mensch</span>, und <span className={styles.introTools}>Methoden</span>, um den Übergang zur New
+              Work in wissenschaftlichen Einrichtungen zu begleiten.
+            </>
+          }
           credit="Illustration von Max Köhler"
         />
         <section className={styles.content}>
