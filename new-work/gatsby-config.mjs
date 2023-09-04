51d49f3186d0124e354ba63964408ab4b9abebff
Max Kohler <hello@maxkohler.com>
2023-09-04 22:27:44 +0200

    Remove logging

--- new-work/gatsby-config.mjs
+++ new-work/gatsby-config.mjs
@@ -4,7 +4,7 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 import remarkGfm from 'remark-gfm'
 import adapter from 'gatsby-adapter-netlify'
-console.log(adapter)
+
 const config = {
   siteMetadata: {
     siteUrl: `https://leibniz-nw.netlify.app`,
