ef41d3e75e38d7baefe2d2f0de76311b2de9673a
Christian Kehres <c.kehres@wus.agency>
2023-09-13 10:59:51 +0200

    code formatting

--- e-learning/src/components/MapLegend.js
+++ e-learning/src/components/MapLegend.js
@@ -1,48 +1,36 @@
-import * as styles from "./LayeredMap.module.scss";
-import React from "react";
-import MarkerMapLayer from "./MarkerLayer";
-import CountryStatisticsLayer from "./CountryStatisticsLayer";
+import * as styles from './LayeredMap.module.scss'
+import React from 'react'
+import MarkerMapLayer from './MarkerLayer'
+import CountryStatisticsLayer from './CountryStatisticsLayer'
 
-export default function MapLegend({
-                                      children
-                                  }) {
+export default function MapLegend({ children }) {
+  return (
+    <div className={styles.legends}>
+      {/** marker Layer */}
+      {React.Children.map(children, (child) => {
+        if (child.type === MarkerMapLayer) {
+          return (
+            <div className={styles.legend}>
+              {React.cloneElement(child, {
+                renderLegend: true,
+              })}
+            </div>
+          )
+        }
+      })}
 
-    return <div className={styles.legends}>
-        {/** marker Layer */}
-        {React.Children.map(children, (child) => {
-            if (child.type === MarkerMapLayer) {
-                return (
-                    <div className={styles.legend}>
-
-                        {React.cloneElement(child,
-                            {
-                                renderLegend: true
-                            }
-                        )}
-                    </div>
-
-                )
-            }
-        })}
-
-        {/** country statistics Layer */}
-        {React.Children.map(children, (child) => {
-            if (child.type === CountryStatisticsLayer) {
-
-                return (
-                    <div className={styles.legend}>
-
-                        {React.cloneElement(child,
-                            {
-                                renderLegend: true
-                            }
-                        )}
-                    </div>
-
-                )
-            }
-        })}
-
-
+      {/** country statistics Layer */}
+      {React.Children.map(children, (child) => {
+        if (child.type === CountryStatisticsLayer) {
+          return (
+            <div className={styles.legend}>
+              {React.cloneElement(child, {
+                renderLegend: true,
+              })}
+            </div>
+          )
+        }
+      })}
     </div>
+  )
 }
