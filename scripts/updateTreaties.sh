4005c590eacf5faf9657cf2808c641b079f1c376
awesomephant <awesomephant@users.noreply.github.com>
2023-09-17 12:14:52 +0200

    Simplify treaty update workflow
    No need to pull the whole repo twice, we can just make a copy of the file we want to compare.

--- scripts/updateTreaties.sh
+++ scripts/updateTreaties.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-base_dir=${GITHUB_WORKSPACE:="../"}
+base_dir=${GITHUB_WORKSPACE:="."}
 branch_name=${BRANCH_NAME:="test"}
 github_actor=${GITHUB_ACTOR:="awesomephant"}
 
-cd ${base_dir}/updates
-
-npm ci
+# npm ci
 
 git config --local user.name "${github_actor}"
 git config --local user.email "${github_actor}@users.noreply.github.com"
+
+mkdir ${base_dir}/tmp -p
+cp ${base_dir}/e-learning/content/data/treaties.json ${base_dir}/tmp/treaties-old.json
 
 echo "Checking if ${branch_name} exists on remote..."
 git ls-remote --exit-code --quiet --heads origin ${branch_name}
@@ -23,24 +24,22 @@ if ((branch_exists == 0)); then
     node ./e-learning/utils/updateTreaties.mjs
     treaties_changed=$?
     if ((treaties_changed == 0)); then
-        node ${base_dir}/updates/scripts/generateTreatiesMessage.js --head=${base_dir}/updates/e-learning/content/data/treaties.json --base=${base_dir}/main/e-learning/content/data/treaties.json --countryData=${base_dir}/updates/e-learning/content/data/countries.json
+        node ${base_dir}/scripts/generateTreatiesMessage.js --head=${base_dir}/e-learning/content/data/treaties.json --base=${base_dir}/tmp/treaties-old.json --countryData=${base_dir}/e-learning/content/data/countries.json
         git add --all
         git commit -m "Update treaty participants"
         git push
-        gh pr edit --body-file ${base_dir}/updates/tmp/pr.md
+        gh pr edit --body-file ${base_dir}/tmp/pr.md
     fi
 else
     echo "Branch ${branch_name} not found, creating a new one..."
     git checkout -b ${branch_name}
-    node ${base_dir}/updates/e-learning/utils/updateTreaties.mjs
+    node ${base_dir}/e-learning/utils/updateTreaties.mjs
     treaties_changed=$?
     if ((treaties_changed == 0)); then
-        node ${base_dir}/updates/scripts/generateTreatiesMessage.js --head=${base_dir}/updates/e-learning/content/data/treaties.json --base=${base_dir}/main/e-learning/content/data/treaties.json --countryData=${base_dir}/updates/e-learning/content/data/countries.json
+        node ${base_dir}/scripts/generateTreatiesMessage.js --head=${base_dir}/e-learning/content/data/treaties.json --base=${base_dir}/tmp/treaties-old.json --countryData=${base_dir}/e-learning/content/data/countries.json
         git add --all
         git commit -m "Update treaty participants"
         git push -u origin ${branch_name}
-        gh pr create -B main -H ${branch_name} --title 'Update treaty participants' --body-file ${base_dir}/updates/tmp/pr.md --label "e-learning"
+        gh pr create -B main -H ${branch_name} --title 'Update treaty participants' --body-file ${base_dir}/tmp/pr.md --label "e-learning"
     fi
-    # git checkout main
-    # git branch -d ${branch_name}
 fi
