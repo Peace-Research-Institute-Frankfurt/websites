c0140642730febbd69cec0c96a06bb12c39f819f
awesomephant <hello@maxkohler.com>
2023-10-04 11:09:46 +0200

    Fix typos

--- new-work/src/pages/index.js
+++ new-work/src/pages/index.js
@@ -68,7 +68,7 @@ const Index = ({ data }) => {
           intro={
             <>
               New Work bricht Regeln auf, hinterfragt Machtverhältnisse und rückt den Menschen seinen Stärken, Bedürfnissen und Emotiononen in den
-              Fokus. Diese Handreichung sammelt Anleitungen und Resourcen zu <span className={styles.introSpace}>Raum</span>,{' '}
+              Fokus. Diese Handreichung sammelt Anleitungen und Ressourcen zu <span className={styles.introSpace}>Raum</span>,{' '}
               <span className={styles.introPeople}>Mensch</span>, und <span className={styles.introTools}>Methoden</span>, um den Übergang zur New
               Work in wissenschaftlichen Einrichtungen zu begleiten.
             </>
