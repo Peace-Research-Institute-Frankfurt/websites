a1277a8615e163e89eef8c9add464dcd8f6e56d1
Max Kohler <hello@maxkohler.com>
2024-03-15 16:24:44 +0100

    Closes #559

--- cntr-monitor/src/components/Post.js
+++ cntr-monitor/src/components/Post.js
@@ -248,7 +248,7 @@ export function Head({ data, pageContext, location }) {
     translations: data.translations.nodes,
   }
   const { primary, dark, light, knockout } = useColors(
-    data.post.childMdx.frontmatter.category === 'Analyse' || data.post.childMdx.frontmatter.category === 'Analysis'
+    ['Analyse', 'Analysis', 'Anhang', 'Appendix'].includes(data.post.childMdx.frontmatter.category)
       ? 'rgb(0, 106, 140)'
       : data.issue.childMdx.frontmatter.color
   )
