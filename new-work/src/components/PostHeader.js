530a941a78c35219ab2b04ed25588e914c7de4eb
awesomephant <hello@maxkohler.com>
2024-05-28 11:13:39 +0200

    Don't panic when no header credits are passed

--- new-work/src/components/PostHeader.js
+++ new-work/src/components/PostHeader.js
@@ -3,14 +3,16 @@ import * as styles from './PostHeader.module.scss'
 import MarkdownRenderer from 'react-markdown-renderer'
 
 export default function PostHeader({ title, intro, hasIllustration, media, portrait, credits }) {
-  const creditEls = credits.map((el, i) => {
-    return (
-      <li key={`credit.${i}`}>
-        <span className={styles.creditsLabel}>{el.label || 'Illustration'}</span>
-        <MarkdownRenderer markdown={el.name} />
-      </li>
-    )
-  })
+  const creditEls = credits
+    ? credits.map((el, i) => {
+        return (
+          <li key={`credit.${i}`}>
+            <span className={styles.creditsLabel}>{el.label || 'Illustration'}</span>
+            <MarkdownRenderer markdown={el.name} />
+          </li>
+        )
+      })
+    : false
 
   return (
     <header className={`${styles.wrapper} ${media ? styles.hasMedia : ''} ${!hasIllustration ? styles.hasDefault : ''}`}>
@@ -18,7 +20,7 @@ export default function PostHeader({ title, intro, hasIllustration, media, portr
       <div className={`${styles.container}`}>
         <div className={styles.titleContainer}>
           <h1 className={`${styles.title} ${title.length > 40 ? styles.isLong : ''}`}>{title}</h1>
-          {creditEls.length > 0 && <ul className={styles.credits}>{creditEls}</ul>}
+          {creditEls && <ul className={styles.credits}>{creditEls}</ul>}
         </div>
         {intro && (
           <div className={`${styles.intro} ${portrait ? styles.hasPortrait : ''}`}>
