c96c1e0a634ad7cc6dc17a8975511a4146545cb1
awesomephant <hello@maxkohler.com>
2024-08-06 10:53:43 +0200

    Add missing alt text

--- new-work/src/components/EventBanner.js
+++ new-work/src/components/EventBanner.js
@@ -6,35 +6,39 @@ import { StaticImage } from 'gatsby-plugin-image'
 import Schedule from './Schedule'
 
 export default function EventBanner() {
-	return (
-		<section className={styles.container}>
-			<div className={styles.illustrationContainer}>
-				<StaticImage src={'../images/Rosa-Ahlers_Inner-Work-crop.png'} className={styles.illustration} />
-			</div>
-			<span className={styles.eyebrow}>
-				<StarIcon />
-				Veranstaltung
-			</span>
-			<h2 className={styles.title}>
-				<em>KI und New Work</em>
-				<span>Zukunftsstrategien für Wissenschaftseinrichtungen</span>
-			</h2>
-			<p className={styles.intro}>
-				Neue Entwicklungen wie künstliche Intelligenz und New Work haben großen Einfluss auf wissenschaftliche Einrichtungen. Wie kann es den
-				Instituten gelingen, auf diese rasanten Veränderungen zu reagieren? Welche Chancen bieten innovative Technologien und moderne Arbeitskonzepte
-				für die Gestaltung der Berufswelt von morgen?
-			</p>
-			<div className={styles.details}>
-				<p>26. September 2024, 11:45–17:30</p>
-				<p>Senckenberg Forschungsinstitut und Naturmuseum Frankfurt, Festsaal Jügelhaus </p>
-			</div>
-			<Schedule to="2024-09-17">
-				<div className={styles.cta}>
-					<a href="https://eveeno.com/100759422">
-						Jetzt anmelden <ArrowRightIcon />
-					</a>
-				</div>
-			</Schedule>
-		</section>
-	)
+  return (
+    <section className={styles.container}>
+      <div className={styles.illustrationContainer}>
+        <StaticImage
+          src={'../images/Rosa-Ahlers_Inner-Work-crop.png'}
+          className={styles.illustration}
+          alt="Illustration mit zwei Personen; eine meditierend mit geschlossenen Augen und erhobenen Händen, die zweite geht durch eine Tür in der Brust der ersten."
+        />
+      </div>
+      <span className={styles.eyebrow}>
+        <StarIcon />
+        Veranstaltung
+      </span>
+      <h2 className={styles.title}>
+        <em>KI und New Work</em>
+        <span>Zukunftsstrategien für Wissenschaftseinrichtungen</span>
+      </h2>
+      <p className={styles.intro}>
+        Neue Entwicklungen wie künstliche Intelligenz und New Work haben großen Einfluss auf wissenschaftliche Einrichtungen. Wie kann es den
+        Instituten gelingen, auf diese rasanten Veränderungen zu reagieren? Welche Chancen bieten innovative Technologien und moderne Arbeitskonzepte
+        für die Gestaltung der Berufswelt von morgen?
+      </p>
+      <div className={styles.details}>
+        <p>26. September 2024, 11:45–17:30</p>
+        <p>Senckenberg Forschungsinstitut und Naturmuseum Frankfurt, Festsaal Jügelhaus </p>
+      </div>
+      <Schedule to="2024-09-17">
+        <div className={styles.cta}>
+          <a href="https://eveeno.com/100759422">
+            Jetzt anmelden <ArrowRightIcon />
+          </a>
+        </div>
+      </Schedule>
+    </section>
+  )
 }
