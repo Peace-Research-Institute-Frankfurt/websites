1692089783f85947ae699553239ec1f0ff8ffba9
awesomephant <hello@maxkohler.com>
2024-08-08 18:42:38 +0200

    EventBanner: Update copy

--- new-work/src/components/EventBanner.js
+++ new-work/src/components/EventBanner.js
@@ -17,25 +17,26 @@ export default function EventBanner() {
       </div>
       <span className={styles.eyebrow}>
         <StarIcon />
-        Veranstaltung
+        Tagung
       </span>
       <h2 className={styles.title}>
         <em>KI und New Work</em>
         <span>Zukunftsstrategien für Wissenschaftseinrichtungen</span>
       </h2>
       <p className={styles.intro}>
-        Neue Entwicklungen wie künstliche Intelligenz und New Work haben großen Einfluss auf wissenschaftliche Einrichtungen. Wie kann es den
-        Instituten gelingen, auf diese rasanten Veränderungen zu reagieren? Welche Chancen bieten innovative Technologien und moderne Arbeitskonzepte
-        für die Gestaltung der Berufswelt von morgen?
+        Am 26. September findet unser dritter WorkNew Day zum Thema KI und New Work statt. Kommen Sie nach Frankfurt und diskutieren Sie mit uns, wie
+        Künstliche Intelligenz und neue Arbeitsformen Forschung und Verwaltung verändern werden. Wir freuen uns auf Sie, auf inspirierende Vorträge
+        und den feierlichen Abschluss des Projekts WorkNew@leibniz!
       </p>
       <div className={styles.details}>
         <p>26. September 2024, 11:45–17:30</p>
         <p>Senckenberg Forschungsinstitut und Naturmuseum Frankfurt, Festsaal Jügelhaus </p>
+        <p>Anmeldung bis 17. September 2024 · Die Veranstaltung ist kostenfrei.</p>
       </div>
       <Schedule to="2024-09-17">
         <div className={styles.cta}>
           <a href="https://eveeno.com/100759422">
-            Jetzt anmelden <ArrowRightIcon />
+            Anmeldung und Agenda <ArrowRightIcon />
           </a>
         </div>
       </Schedule>
