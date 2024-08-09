fddc4f1c3cbfe80e5d799f41b816542e0042ce55
awesomephant <hello@maxkohler.com>
2024-08-09 16:53:57 +0200

    Build post-soviet and yekaterinburg maps for #761

--- e-learning/src/components/MarkerLayerMarkers.js
+++ e-learning/src/components/MarkerLayerMarkers.js
@@ -1,36 +1,40 @@
 import * as styles from './LayeredMap.module.scss'
 import React from 'react'
 
-export default function MarkerLayerMarkers({ data, projection }) {
-  const convertDMSToDD = (coordinate) => {
-    let dmsParts = coordinate.split(/[^\d\w]+/)
-    let degrees = dmsParts[0]
-    let minutes = dmsParts[1]
-    let seconds = dmsParts[2]
-    let direction = dmsParts[3]
+const convertDMSToDD = (coordinate) => {
+  const dmsParts = coordinate.split(/[^\d\w]+/)
+  const degrees = dmsParts[0]
+  const minutes = dmsParts[1]
+  const seconds = dmsParts[2]
+  const direction = dmsParts[3]
 
-    let dd = Number(degrees) + Number(minutes) / 60 + Number(seconds) / (60 * 60)
+  let dd = Number(degrees) + Number(minutes) / 60 + Number(seconds) / (60 * 60)
 
-    if (direction === 'S' || direction === 'W') {
-      dd = dd * -1
-    }
-
-    return dd
+  if (direction === 'S' || direction === 'W') {
+    dd = dd * -1
   }
 
+  return dd
+}
+
+export default function MarkerLayerMarkers({ data, projection }) {
   return (
     <g>
-      {data.map((marker, i) => (
-        <circle
-          key={`markerCircle.${i}`}
-          r={8}
-          className={styles.marker}
-          transform={`translate(${projection.projection([
-            Number.isNaN(Number(marker.long)) ? convertDMSToDD(marker.long) : Number(marker.long),
-            Number.isNaN(Number(marker.lat)) ? convertDMSToDD(marker.lat) : Number(marker.lat),
-          ])})`}
-        />
-      ))}
+      {data
+        .filter((el) => {
+          return el.lat && el.long
+        })
+        .map((marker, i) => (
+          <circle
+            key={`markerCircle.${i}`}
+            r={8}
+            className={styles.marker}
+            transform={`translate(${projection.projection([
+              Number.isNaN(Number(marker.long)) ? convertDMSToDD(marker.long) : Number(marker.long),
+              Number.isNaN(Number(marker.lat)) ? convertDMSToDD(marker.lat) : Number(marker.lat),
+            ])})`}
+          />
+        ))}
     </g>
   )
 }
