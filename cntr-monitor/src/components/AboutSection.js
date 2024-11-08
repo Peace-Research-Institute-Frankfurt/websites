aefbc9174a1487aa1b6a1dd166d2f6c220d82dc7
Elisabeth Waczek <140592956+EWaczek@users.noreply.github.com>
2024-11-08 10:39:53 +0100

    update about section

--- cntr-monitor/src/components/AboutSection.js
+++ cntr-monitor/src/components/AboutSection.js
@@ -9,7 +9,7 @@ const AboutSection = () => {
       <h2 className={styles.title}>{t('About')}</h2>
       <p className={styles.copy}>
         {t(
-          "The CNTR Monitor is an open-access publication on trends on technology and arms control and the core product of CNTR's research. In addition to analyses of individual technologies and reports on Research & Development (R&D) activities, the CNTR Monitor highlights opportunities for political control and capacities in selected regions."
+          "The CNTR Monitor is an annual open-access publication and the core product of CNTR's research. It highlights technological innovations and developments in the natural sciences that are relevant to peace and security. In addition to analyses of individual technologies and reports on research and development (R&D) activities, the Monitor also explores opportunities for arms control and outlines options for political regulation."
         )}
       </p>
     </div>
